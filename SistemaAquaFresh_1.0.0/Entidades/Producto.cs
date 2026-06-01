a6d08486b026b9c410b42e1f37e07d02b4c432cf
aljojicorr01-crypto <aljojicorr01@gmail.com>
2026-06-01 18:27:36 -0500

    feat: agregar validacion de precio negativo en clase Producto

--- SistemaAquaFresh_1.0.0/Entidades/Producto.cs
+++ SistemaAquaFresh_1.0.0/Entidades/Producto.cs
@@ -8,7 +8,7 @@ namespace SistemaAquaFresh_1._0._0.Entidades
         private string nombre;
         private string tipo_envase;
         private decimal precio_unitario;
-
+       
         public Producto() { }
         public Producto(int id_producto, string nombre, string tipo_envase, decimal precio_unitario)
         {
@@ -22,5 +22,15 @@ namespace SistemaAquaFresh_1._0._0.Entidades
         public string Nombre { get => nombre; set => nombre = value?.Trim(); }
         public string Tipo_envase { get => tipo_envase; set => tipo_envase = value?.Trim(); }
         public decimal Precio_unitario { get => precio_unitario; set => precio_unitario = value; }
+
+
+
+        public decimal Precio_unitario
+        {
+             get => precio_unitario;
+             set => precio_unitario = value < 0
+             ? throw new ArgumentException("El precio unitario no puede ser negativo.")
+             : value;
+        }
     }
 }
