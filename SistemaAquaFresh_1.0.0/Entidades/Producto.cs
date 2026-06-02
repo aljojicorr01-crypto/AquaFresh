d6b35867e5b59056278cab8137ec41a2e24da523
aljojicorr01-crypto <aljojicorr01@gmail.com>
2026-06-01 19:35:16 -0500

    fix: corregir propiedad Precio_unitario duplicada

--- SistemaAquaFresh_1.0.0/Entidades/Producto.cs
+++ SistemaAquaFresh_1.0.0/Entidades/Producto.cs
@@ -8,7 +8,7 @@ namespace SistemaAquaFresh_1._0._0.Entidades
         private string nombre;
         private string tipo_envase;
         private decimal precio_unitario;
-       
+
         public Producto() { }
         public Producto(int id_producto, string nombre, string tipo_envase, decimal precio_unitario)
         {
@@ -21,16 +21,12 @@ namespace SistemaAquaFresh_1._0._0.Entidades
         public int Id_producto { get => id_producto; set => id_producto = value; }
         public string Nombre { get => nombre; set => nombre = value?.Trim(); }
         public string Tipo_envase { get => tipo_envase; set => tipo_envase = value?.Trim(); }
-        public decimal Precio_unitario { get => precio_unitario; set => precio_unitario = value; }
-
-
-
         public decimal Precio_unitario
         {
-             get => precio_unitario;
-             set => precio_unitario = value < 0
-             ? throw new ArgumentException("El precio unitario no puede ser negativo.")
-             : value;
+            get => precio_unitario;
+            set => precio_unitario = value < 0
+                ? throw new ArgumentException("El precio unitario no puede ser negativo.")
+                : value;
         }
     }
 }
