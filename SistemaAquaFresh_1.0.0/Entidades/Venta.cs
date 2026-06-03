96d80d150afbad959531259c3f77fa06eb6dc30f
aljojicorr01-crypto <aljojicorr01@gmail.com>
2026-06-02 19:09:04 -0500

    fix: resolver conflicto en clase Venta

--- SistemaAquaFresh_1.0.0/Entidades/Venta.cs
+++ SistemaAquaFresh_1.0.0/Entidades/Venta.cs
@@ -1,18 +1,10 @@
-<<<<<<< HEAD
 using System;
-=======
-﻿using System;
->>>>>>> 4557bed474d35d11258da463b6cd71b24e3171f1
 
 namespace SistemaAquaFresh_1._0._0.Entidades
 {
     public class Venta
     {
         private static int nextId = 1;
-<<<<<<< HEAD
-=======
-
->>>>>>> 4557bed474d35d11258da463b6cd71b24e3171f1
         private int id_venta;
         private Cliente cliente;
         private Producto producto;
@@ -25,10 +17,6 @@ namespace SistemaAquaFresh_1._0._0.Entidades
             if (cliente == null) throw new ArgumentNullException(nameof(cliente), "El cliente no puede ser nulo.");
             if (producto == null) throw new ArgumentNullException(nameof(producto), "El producto no puede ser nulo.");
             if (cantidad <= 0) throw new ArgumentException("La cantidad debe ser mayor que cero.", nameof(cantidad));
-<<<<<<< HEAD
-=======
-
->>>>>>> 4557bed474d35d11258da463b6cd71b24e3171f1
             this.id_venta = nextId++;
             this.Cliente = cliente;
             this.Producto = producto;
@@ -38,28 +26,16 @@ namespace SistemaAquaFresh_1._0._0.Entidades
         }
 
         public int Id_venta { get => id_venta; }
-<<<<<<< HEAD
-=======
-
->>>>>>> 4557bed474d35d11258da463b6cd71b24e3171f1
         public Cliente Cliente
         {
             get => cliente;
             set => cliente = value ?? throw new ArgumentNullException(nameof(Cliente), "El cliente no puede ser nulo.");
         }
-<<<<<<< HEAD
-=======
-
->>>>>>> 4557bed474d35d11258da463b6cd71b24e3171f1
         public Producto Producto
         {
             get => producto;
             set => producto = value ?? throw new ArgumentNullException(nameof(Producto), "El producto no puede ser nulo.");
         }
-<<<<<<< HEAD
-=======
-
->>>>>>> 4557bed474d35d11258da463b6cd71b24e3171f1
         public int Cantidad
         {
             get => cantidad;
@@ -70,19 +46,11 @@ namespace SistemaAquaFresh_1._0._0.Entidades
                 RecalcularTotal();
             }
         }
-<<<<<<< HEAD
-=======
-
->>>>>>> 4557bed474d35d11258da463b6cd71b24e3171f1
         public DateTime FechaVenta
         {
             get => fechaVenta;
             private set => fechaVenta = value;
         }
-<<<<<<< HEAD
-=======
-
->>>>>>> 4557bed474d35d11258da463b6cd71b24e3171f1
         public decimal Total
         {
             get => total;
